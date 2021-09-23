3c804216d76a44123d9aa8f0f466fd917b549206
Ricky Bustillos <henrique.ssbl@hotmail.com>
2021-09-23 10:10:46 -0300

    Spring Boot base configuration

---
+++ src/main/java/br/com/ricky/manageMeetsApi/ManageMeetsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ricky.manageMeetsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageMeetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageMeetsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ricky/manageMeetsApi/ManageMeetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ricky.manageMeetsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageMeetsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
